57e54339b1e9b441d1b6a47209a91a533e7d8c37
Ahmad1566 <130237750+Ahmad1566@users.noreply.github.com>
2024-07-13 15:00:25 +0000

    container config done

---
+++ 01-DAY/text.js
@@ -0,0 +1,35 @@
+console.log("Ahmad");
+
+// Task 01
+let number = 55
+console.log(number);
+
+// Task 02
+let string = "I love Pakistan"
+console.log(string);
+
+// Task 03
+const isAhmadGood = true;
+console.log(isAhmadGood);
+
+// Task 04
+const numm = 42;
+const stringvar = "Hello, World"; 
+const boolean = true;
+const student = { name: "Ahmad", age: 21 };
+const array = [1, 2, 3, 4, 5];
+const nullVar = null;
+const functionVar = function() { return "I'm a function"; };
+console.table([typeof numm, typeof stringvar, typeof boolean, typeof student, typeof array, typeof nullVar, typeof functionVar]) // Array is a special type of Object
+
+// Task 05
+let marks = 50
+console.log(marks)
+marks = 60
+console.log(marks)
+
+// Task 06
+const name = "Ali"
+console.log(name)
+name = "Ahmad";
+console.log(name)
